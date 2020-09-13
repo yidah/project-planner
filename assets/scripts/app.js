4d96cfd56a4de30a61730238dd53f7681f07eb72
Yidah Melendez <ymelendez@bethel.jw.org>
2020-09-13 15:37:15 +0100

    Added base class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,58 @@
-class DOMHelper{
-  static clearEventListeneres(element){
+class DOMHelper {
+  static clearEventListeneres(element) {
     const clonedElement = element.cloneNode(true);
     // Clonning the element with itself we ditch any old event handlers so no memory leaks when adding new event handlers to the button
     element.replaceWith(clonedElement);
     return clonedElement;
-
   }
-    static moveElement(elementId, newDestinationSelector){
-        const element = document.getElementById(elementId);
-        const destinationElement = document.querySelector(newDestinationSelector);
-        destinationElement.append(element);
-
-    }
+  static moveElement(elementId, newDestinationSelector) {
+    const element = document.getElementById(elementId);
+    const destinationElement = document.querySelector(newDestinationSelector);
+    destinationElement.append(element);
+  }
 }
 
-class Tooltip {
+// Adding a base class
+class Component {
+  detached() {
+    if (this.element) {
+      // "this" always refers to the subclass
+      this.element.remove(); //modern syntax
+      //this.element.parentElement.removeChild(this.element); // old syntax it will work in all browsers
+    }
+  }
 
+  attached() {
+    document.body.append(this.element);
+  }
+}
+
+class Tooltip extends Component {
+  constructor(closeNotifierFunction) {
+    super();
+    this.closeNotifier = closeNotifierFunction;
+    this.create();
+  }
+
+  // This is an alternative to bind "this" in tooltipElement.addEventListener('click', this.closeTooltip.bind(this));
+  // with arrow functions "this" aways refers to the class
+  // This is ok for this little funciton but is not the more efficient solution as this arrow function gets created for every isntance of tooltip
+  closeTooltip = () => {
+    this.detached();
+    this.closeNotifier();
+  };
+
+  create() {
+    const tooltipElement = document.createElement('div');
+    tooltipElement.className = 'card';
+    tooltipElement.textContent = 'DUMMY!';
+    tooltipElement.addEventListener('click', this.closeTooltip);
+    this.element = tooltipElement;
+  }
 }
 
 class ProjectItem {
+  hasActiveTooltip = false;
   constructor(id, updateProjectListsFunciton, type) {
     this.id = id;
     this.updateProjectListsHandler = updateProjectListsFunciton;
@@ -26,7 +60,25 @@ class ProjectItem {
     this.connectSwitchButton(type);
   }
 
-  connectMoreInfoButton() {}
+  showMreInfoHandler() {
+    // we do not want to open a tooltip more than once
+    if (this.hasActiveTooltip) {
+      return;
+    }
+    const tooltip = new Tooltip(() => {
+      this.hasActiveTooltip = false;
+    });
+    tooltip.attached();
+    this.hasActiveTooltip = true;
+  }
+
+  connectMoreInfoButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const moreInfoBtn = projectItemElement.querySelector(
+      'button:first-of-type'
+    );
+    moreInfoBtn.addEventListener('click', this.showMreInfoHandler);
+  }
 
   connectSwitchButton(type) {
     const projectItemElement = document.getElementById(this.id);
@@ -34,12 +86,15 @@ class ProjectItem {
     // In case this button had already an event handler we clear it to avoid memory leaks and errors
     switchBtn = DOMHelper.clearEventListeneres(switchBtn);
     // If project was moved update button text
-    switchBtn.textContent = type === 'active' ? 'Finish':'Activate';
+    switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
     // updateProjectListsHandler points to the switchProject function in ProjectList class
-    switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null,this.id));
+    switchBtn.addEventListener(
+      'click',
+      this.updateProjectListsHandler.bind(null, this.id)
+    );
   }
 
-  update(updateProjectListsFun, type){
+  update(updateProjectListsFun, type) {
     this.updateProjectListsHandler = updateProjectListsFun;
     this.connectSwitchButton(type);
   }
@@ -52,7 +107,9 @@ class ProjectList {
     this.type = type;
     const prjItems = document.querySelectorAll(`#${this.type}-projects li`);
     for (const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type)
+      );
     }
     console.log(this.projects);
   }
@@ -62,12 +119,11 @@ class ProjectList {
   }
 
   addProject(project) {
-      // "this" was set in init() and corresponds not to the list that the project currently belongs but the other
-      this.projects.push(project);
-      DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
-      // We update the project with a new handler as now it belongs to the "this" list
-      project.update(this.switchProject.bind(this), this.type);
-
+    // "this" was set in init() and corresponds not to the list that the project currently belongs but the other
+    this.projects.push(project);
+    DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
+    // We update the project with a new handler as now it belongs to the "this" list
+    project.update(this.switchProject.bind(this), this.type);
   }
 
   // This method is sent as the function to be called when activate or finished button is click
@@ -82,12 +138,12 @@ class App {
   static init() {
     const activeProjectsList = new ProjectList('active');
     const finishedProjectsList = new ProjectList('finished');
-    // Active projects need to be added to the finished list the handler will point to the addProject funciton 
+    // Active projects need to be added to the finished list the handler will point to the addProject funciton
     // and "this" will be de finishedProjectsList when the function is called
     activeProjectsList.setSwitchHandlerFunction(
       finishedProjectsList.addProject.bind(finishedProjectsList)
     );
-    // Finished projects need to be added to the active list the handler will point to the addProject funciton 
+    // Finished projects need to be added to the active list the handler will point to the addProject funciton
     // and "this" will be de activeProjectsList when the function is called
     finishedProjectsList.setSwitchHandlerFunction(
       activeProjectsList.addProject.bind(activeProjectsList)
